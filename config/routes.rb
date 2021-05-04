a41ec72ae0ea6aec278673cc22f1eb41f3e21373
mayumiyamada <y.mayumi1011@gmail.com>
2021-05-04 14:09:21 +0900

    シンプルスケジュール

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   resources :users, only: [:edit, :update, :show]
   resources :tweets
+  resources :calendars
   resources :rooms, only: [:index, :new, :create, :destroy] do
   resources :messages, only: [:index, :create]
+ 
   end
 end
